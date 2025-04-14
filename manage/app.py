574dd30f405b22b47d14ae7a0fcbe358f932bc53
Malcolm Baig <git@baig.org>
2025-04-14 11:47:56 +0100

    Add markup for clinics page
    Includes:
    
    - Modifications to the jinja environment to use some custom filters
    - A layout template for use by all views in this project
    - The clinics view itself, adapted from the prototype
    - Stubbed out clinics data

--- manage/app.py
+++ manage/app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import manage.jinja_filters as jinja_filters
 
 app = Flask(__name__)
 
+# Register Jinja filters
+app.jinja_env.filters['noWrap'] = jinja_filters.no_wrap
+app.jinja_env.filters['asHint'] = jinja_filters.as_hint
+app.jinja_env.filters['formatWords'] = jinja_filters.format_words
+app.jinja_env.filters['formatDate'] = jinja_filters.format_date
+app.jinja_env.filters['formatDateTime'] = jinja_filters.format_date_time
+app.jinja_env.filters['formatTime'] = jinja_filters.format_time
+app.jinja_env.filters['formatTimeRange'] = jinja_filters.format_time_range
+
 @app.route("/")
 def index():
-    return "Home"
+    return redirect("/clinics")
 
 @app.route("/clinics")
 def clinics():
-    return render_template("clinics.html")
+    return render_template("clinics.html", filteredClinicCounts={
+        'today': 0,
+        'upcoming': 0,
+        'completed': 0,
+        'all': 0
+    })
