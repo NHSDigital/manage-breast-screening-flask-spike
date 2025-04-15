e95adc40277d3d242dc125e5c3bab204881fa988
Malcolm Baig <git@baig.org>
2025-04-15 14:41:55 +0100

    Use test clinic data to mock up the Clinics page
    Create a rough representation of the clinics page from the prototype
    using the test clinic data we added via migrations.

--- manage/clinics.py
+++ manage/clinics.py
@@ -1,15 +1,16 @@
-import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from manage.models import Clinic
 
 bp = Blueprint('clinics', __name__, url_prefix='/clinics')
 
 @bp.route("/")
 def clinics():
+    filtered_clinics = Clinic.query.all()
     return render_template("clinics.html", filteredClinicCounts={
         'today': 0,
         'upcoming': 0,
         'completed': 0,
         'all': 0
-    })
+    }, filteredClinics=filtered_clinics)
